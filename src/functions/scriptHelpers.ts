d7b3406f799e027eb6fbf788144960301d6049f0
James <5511220+Zamiell@users.noreply.github.com>
2023-10-25 10:54:25 -0400

    refactor!: use arktype library for better path detection

--- src/functions/scriptHelpers.ts
+++ src/functions/scriptHelpers.ts
@@ -1,45 +1,36 @@
-import { assertDefined, capitalizeFirstLetter } from "isaacscript-common-ts";
+// See: https://github.com/arktypeio/arktype/blob/beta/ark/fs/fs.ts
+// Useful functions:
+// - dirName
+// - findPackageRoot
+
+import { findPackageRoot } from "@arktype/fs";
+import { capitalizeFirstLetter } from "isaacscript-common-ts";
 import path from "node:path";
 import type { Options } from "zx";
-import {
-  getClosestPackageJSONDir,
-  getDirname,
-  getElapsedSeconds,
-} from "./utils.js";
-
-type ScriptType = "build" | "lint" | "test";
-
-const SCRIPT_TYPE_VERBS = {
-  build: "built",
-  lint: "linted",
-  test: "tested",
-} as const satisfies Record<ScriptType, string>;
+import { getElapsedSeconds } from "./utils.js";
 
 /** See the documentation for the `script` helper function. */
 export async function buildScript(
   $: Options,
-  importMetaURL: string,
-  func: () => Promise<void>,
+  func: () => Promise<void> | void,
 ): Promise<void> {
-  await script($, importMetaURL, func, "build");
+  await script($, func, "building", "built");
 }
 
 /** See the documentation for the `script` helper function. */
 export async function lintScript(
   $: Options,
-  importMetaURL: string,
-  func: () => Promise<void>,
+  func: () => Promise<void> | void,
 ): Promise<void> {
-  await script($, importMetaURL, func, "lint");
+  await script($, func, "linting", "linted");
 }
 
 /** See the documentation for the `script` helper function. */
 export async function testScript(
   $: Options,
-  importMetaURL: string,
-  func: () => Promise<void>,
+  func: () => Promise<void> | void,
 ): Promise<void> {
-  await script($, importMetaURL, func, "test");
+  await script($, func, "testing", "tested");
 }
 
 /**
@@ -50,35 +41,27 @@ export async function testScript(
  * of a Bash script.
  *
  * @param $ The global variable from `zx`. (This is used to turn verbosity off.)
- * @param importMetaURL Should always be `import.meta.url`. (This is used to find the root directory
- *                      of the package.)
  * @param func The function that contains the build logic for the particular script.
- * @param scriptType Optional. The kind of script. Omit this if you do not want logging.
+ * @param beforeVerb Optional. The verb for what the script will be doing. For example, "building".
+ * @param afterVerb Optional. The verb for when the script completes. For exampled, "built".
  */
 export async function script(
   $: Options,
-  importMetaURL: string,
-  func: () => Promise<void>,
-  scriptType?: ScriptType,
+  func: () => Promise<void> | void,
+  beforeVerb?: string,
+  afterVerb?: string,
 ): Promise<void> {
   $.verbose = false; // eslint-disable-line no-param-reassign
 
   const args = process.argv.slice(2);
   const quiet = args.includes("quiet");
 
-  const __dirname = getDirname(importMetaURL);
-  const packageRoot = getClosestPackageJSONDir(__dirname);
-  assertDefined(
-    packageRoot,
-    'Failed to find the root directory for the package. Does this project have a "package.json" file?',
-  );
-
+  const packageRoot = findPackageRoot();
   const packageName = path.basename(packageRoot);
 
-  if (!quiet && scriptType !== undefined) {
-    const capitalizedScriptType = capitalizeFirstLetter(scriptType);
-    const verb = `${capitalizedScriptType}ing`;
-    console.log(`${verb}: ${packageName}`);
+  if (!quiet && beforeVerb !== undefined) {
+    const capitalizedVerb = capitalizeFirstLetter(beforeVerb);
+    console.log(`${capitalizedVerb}: ${packageName}`);
   }
 
   process.chdir(packageRoot);
@@ -86,11 +69,10 @@ export async function script(
   const startTime = Date.now();
   await func();
 
-  if (!quiet && scriptType !== undefined) {
-    const verb = SCRIPT_TYPE_VERBS[scriptType];
+  if (!quiet && afterVerb !== undefined) {
     const elapsedSeconds = getElapsedSeconds(startTime);
     console.log(
-      `Successfully ${verb} ${packageName} in ${elapsedSeconds} seconds.`,
+      `Successfully ${afterVerb} ${packageName} in ${elapsedSeconds} seconds.`,
     );
   }
 }
